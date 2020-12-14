7167f79e3dbe961cda9c916d30b87af31c3b5af5
avalladaresm <avalladaresm@unitec.edu>
2020-12-14 03:11:37 -0600

    Added a new test that checks if the leaving date is greater than the starting date to pass

--- features/leavingDateGreatThanEntryDate.feature
+++ features/leavingDateGreatThanEntryDate.feature
@@ -4,6 +4,10 @@ Feature: Setting entry and leaving date and time
 
     Given I go to the website
     When I select <parking_lot>
+    And I set an entry date and time
+    And I set a leaving date and time
+    And I press the Calculate button
+    Then the leaving date and time should be after the entry date and time
 
     Examples:
       | parking_lot                 |
--- features/step-definitions/selectingOptions.steps.js
+++ features/step-definitions/selectingOptions.steps.js
@@ -1,4 +1,4 @@
-const { When } = require('cucumber')
+const { When, Then } = require('cucumber')
 
 When('I select {string}', async (parking_lot) => {
     const elem = await $('#ParkingLot');
@@ -8,4 +8,27 @@ When('I select {string}', async (parking_lot) => {
 When('I press the Calculate button', async () => {
     const elem = await $('input[type="submit"]');
     await elem.click();
+})
+
+const entry = {date: '12/15/2020', time: '12:10'};
+
+When('I set an entry date and time', async () => {
+  const dateElem = await $('input[name="StartingDate"]');
+  const timeElem = await $('input[name="StartingTime"]');
+  dateElem.setValue(entry.date);
+  timeElem.setValue(entry.time);
+})
+
+const leaving = {date: '12/17/2020', time: '14:10'};
+
+When('I set a leaving date and time', async () => {
+  const dateElem = await $('input[name="LeavingDate"]');
+  const timeElem = await $('input[name="LeavingTime"]');
+  dateElem.setValue(leaving.date);
+  timeElem.setValue(leaving.time);
+})
+
+Then('the leaving date and time should be after the entry date and time', async () => {
+    const elem = await $('form table tbody tr:nth-of-type(4) .SubHead b');
+    await expect(elem).not.toHaveText('ERROR! YOUR LEAVING DATE OR TIME IS BEFORE YOUR STARTING DATE OR TIME');
 })
