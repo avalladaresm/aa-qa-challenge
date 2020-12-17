149bd9ed92d15ff9903a522de504cbad8110792a
avalladaresm <avalladaresm@unitec.edu>
2020-12-17 11:21:36 -0600

    Added selenium chrome driver options for travis ci integration

--- wdio.conf.js
+++ wdio.conf.js
@@ -110,7 +110,25 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['selenium-standalone'],
+    services: [
+        [
+            'selenium-standalone', { 
+                drivers: { 
+                    chrome: true 
+                }, 
+                installArgs: {
+                    chrome: {
+                        version: '83.0.4103.116'
+                    }
+                }, 
+                args: {
+                    chrome: {
+                        version: '83.0.4103.116'
+                    }
+                }
+            }
+        ]
+    ],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
